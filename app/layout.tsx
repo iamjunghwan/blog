0c0ca19217c16e6f94ed493227893e26a0a76fd2
iamjunghwan <dks3344@naver.com>
2025-02-21 10:35:15 +0900

    opengraph test

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export async function generateMetadata() {
       siteName: "Example Blog",
       images: [
         {
-          url: "http://13.209.15.220:3000/iaman.jpeg",
+          url: "http://43.200.3.68:3000/iaman.jpeg",
           width: 800,
           height: 600,
           alt: "Blog Post Image",
