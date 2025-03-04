12bf254c836c328e7316fe1fb7f80a8b779a571d
iamjunghwan <dks3344@naver.com>
2025-03-04 09:16:32 +0900

    sitemap change

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,7 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://iaman.kr",
   generateRobotsTxt: true, // (optional)
+  sitemapSize: 7000,
+  lastmod: true,
   // ...other options
 };
