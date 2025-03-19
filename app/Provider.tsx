4693c7e80fdd676cc97f88d7f4920bf3f477af85
iamjunghwan <dks3344@naver.com>
2025-03-19 15:05:54 +0900

    test

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -3,19 +3,18 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-function getQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60 * 1000 * 15,
-        gcTime: 1000 * 60 * 30,
-        refetchOnMount: false,
-      },
-    },
-  });
-}
-
 const Provider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  function getQueryClient() {
+    return new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 60 * 1000 * 15,
+          gcTime: 1000 * 60 * 30,
+          refetchOnMount: false,
+        },
+      },
+    });
+  }
   const queryClient = getQueryClient();
 
   return (
