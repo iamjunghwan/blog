c94f458a05bbe4f59ad1364c3278f32cdc5b5701
iamjunghwan <dks3344@naver.com>
2025-03-24 20:59:35 +0900

    refactor

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -3,20 +3,20 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const Provider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  function getQueryClient() {
-    return new QueryClient({
-      defaultOptions: {
-        queries: {
-          staleTime: 60 * 1000 * 15,
-          gcTime: 1000 * 60 * 30,
-          refetchOnMount: false,
-        },
+function getQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000 * 15,
+        gcTime: 1000 * 60 * 30,
+        refetchOnMount: false,
       },
-    });
-  }
-  const queryClient = getQueryClient();
+    },
+  });
+}
+const queryClient = getQueryClient();
 
+const Provider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <main style={{ marginBottom: "auto" }}>{children}</main>
