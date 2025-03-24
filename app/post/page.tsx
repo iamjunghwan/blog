3b6a2c538043c4540f81bd826172227bc254a3cb
iamjunghwan <dks3344@naver.com>
2025-03-24 13:04:45 +0900

    media

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 
+import "@/app/globals.css";
 import dayjs from "dayjs";
 import InnerHeader from "@/components/InnerHeader";
 import { PostData } from "@/type/index";
 import useQueryData from "@/hooks/useQueryData";
+import DataLoading from "@/components/DataLoading";
 
 export default function Page() {
   const { isPending, error, postData, isFetching } = useQueryData({
     queryKeyName: ["PostsInfo"],
   });
 
-  if (isPending) return "Loading...";
-
+  if (isPending) return <DataLoading />;
   if (error) return "An error has occurred: " + error.message;
 
   return (
@@ -31,15 +32,8 @@ export default function Page() {
               style={{ paddingTop: "2rem", paddingBottom: "3rem" }}
             >
               <article>
-                <div
-                  style={{
-                    display: "grid",
-                    gridTemplateColumns: "repeat(2, minmax(0, 1fr))", // 2분할로 수정
-                    alignItems: "baseline",
-                    gap: "10px",
-                  }}
-                >
-                  <dl style={{ marginLeft: "120px" }}>
+                <div className="postInnerArea">
+                  <dl>
                     <dd style={{ color: "gray" }}>
                       <time dateTime={item.createdAt}>
                         {dayjs(item.createdAt).format("YYYY-MM-DD")}
@@ -50,7 +44,6 @@ export default function Page() {
                     style={{
                       fontWeight: 700,
                       paddingTop: ".5rem",
-                      paddingLeft: "2rem",
                       margin: 0, // h2의 기본 마진 제거
                     }}
                   >
