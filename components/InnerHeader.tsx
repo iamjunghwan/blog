60d7c6e7adce0be374fcd7cd51aa1c6e3bdbefdc
iamjunghwan <dks3344@naver.com>
2025-04-06 14:21:52 +0900

    LCP test

--- components/InnerHeader.tsx
+++ components/InnerHeader.tsx
@@ -2,7 +2,7 @@ export default function InnerHeader({ title }: { title: string }) {
   return (
     <>
       <div className="main ">
-        <h1 className="mainH1">{title}</h1>
+        <h1 style={{ fontSize: "3rem", fontWeight: "900" }}>{title}</h1>
       </div>
       <hr />
     </>
