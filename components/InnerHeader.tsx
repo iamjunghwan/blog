6bc2e559206768c6391a51cdc07c696e5935053f
iamjunghwan <dks3344@naver.com>
2025-04-11 14:32:39 +0900

    🔥 Refactoring

--- components/InnerHeader.tsx
+++ components/InnerHeader.tsx
@@ -2,7 +2,7 @@ export default function InnerHeader({ title }: { title: string }) {
   return (
     <>
       <div className="main">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-black">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-black text-center">
           {title}
         </h1>
       </div>
