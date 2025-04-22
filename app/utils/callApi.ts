362ef11cbc601074591ff09c02e21d6c8039ed20
iamjunghwan <dks3344@naver.com>
2025-04-22 12:57:00 +0900

    ❗️  api token divide

--- app/utils/callApi.ts
+++ app/utils/callApi.ts
@@ -1,8 +1,6 @@
 import { ApiResponse, ApiResponseError } from "@/type/index";
 const API_URL =
   "https://api.memexdata.io/memex/api/projects/0e9c148b/models/blog/contents/search/v2";
-const API_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJSRUJFTDkiLCJpYXQiOjE3Mzk3NzcxMzUsImV4cCI6MjA1NTEzNzEzNSwiSUQiOiIwZTljMTQ4YiIsIkRPTUFJTiI6WyIqIl0sIlRZUEUiOiJFWFRFUk5BTCJ9.i-PuX7QzNpJiqncP06Tc5FyDbFpAg11D-W5csSTdRkg";
 
 export async function callApi(): Promise<
   { list: ApiResponse[] } | ApiResponseError
@@ -12,7 +10,7 @@ export async function callApi(): Promise<
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Access-Token": API_TOKEN,
+        "Access-Token": `${process.env.API_TOKEN}`,
       },
       cache: "force-cache",
       body: JSON.stringify({
--- scripts/callApiForCommonjs.js
+++ scripts/callApiForCommonjs.js
@@ -1,15 +1,14 @@
 const API_URL =
   "https://api.memexdata.io/memex/api/projects/0e9c148b/models/blog/contents/search/v2";
-const API_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJSRUJFTDkiLCJpYXQiOjE3Mzk3NzcxMzUsImV4cCI6MjA1NTEzNzEzNSwiSUQiOiIwZTljMTQ4YiIsIkRPTUFJTiI6WyIqIl0sIlRZUEUiOiJFWFRFUk5BTCJ9.i-PuX7QzNpJiqncP06Tc5FyDbFpAg11D-W5csSTdRkg";
 
+console.log("`${process.env.API_TOKEN}` : ", `${process.env.API_TOKEN}`);
 export async function callApi() {
   try {
     const response = await fetch(API_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Access-Token": API_TOKEN,
+        "Access-Token": `${process.env.API_TOKEN}`,
       },
       cache: "force-cache",
       body: JSON.stringify({
