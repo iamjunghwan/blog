e47d7bc5d2a025f11b40804af276ab3543195986
iamjunghwan <dks3344@naver.com>
2025-05-12 11:17:04 +0900

    🔥 image size

--- components/Card/CardImageArea.tsx
+++ components/Card/CardImageArea.tsx
@@ -33,9 +33,9 @@ const CardImageArea = ({ content }: { content: string }) => {
       <Image
         src={check(content)}
         alt="Post Representative Image"
-        width={100}
-        height={100}
-        className="w-full h-full object-cover"
+        width={96}
+        height={96}
+        className="object-cover rounded-lg"
       />
     </div>
   );
